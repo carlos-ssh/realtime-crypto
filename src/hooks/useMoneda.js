a6773421cda72211bd75f6b93f57f8b15b2d78cd
Carlos <carlos.anezeh@gmail.com>
2022-02-28 12:05:46 -0600

    Fix issues displaying the form

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,4 +1,24 @@
 import React, { Fragment, useState } from 'react';
+import Styled from '@emotion/styled';
+
+const Label = Styled.label`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  margin-top: 2rem;
+  display: block;
+`;
+
+const Select = Styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  -webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.2rem;
+`;
 
 const useMoneda = (label, stateInicial, opciones) => {
 
@@ -6,15 +26,15 @@ const useMoneda = (label, stateInicial, opciones) => {
 
   const Seleccionar = () =>(
     <Fragment>
-      <label>
-        <label>{label}</label>
-        <select>
-          <option value="">-- Selecciona --</option>
-          { opciones.map(opcion => (
-            <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
-          ))}
-        </select>
-      </label>
+      <Label>{label}</Label>
+      <Select
+        onChange={e => actualizarState(e.target.value)}
+      >
+        <option value="">-- Selecciona --</option>
+        { opciones.map(opcion => (
+          <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
+        ))}
+      </Select>
     </Fragment>
   );
 
