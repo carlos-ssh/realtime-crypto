7d722a60a05f0ef84850b71fa5dd053e6723545c
Carlos <carlos.anezeh@gmail.com>
2022-02-28 10:09:09 -0600

    Create stylecomponents for the main page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
+import imagen from './assets/images/img1.png';
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media(min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 7rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66adfe;
+    display: block;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      wertwertwerter
-    </div>
+    <Contenedor>
+      <div>
+        <Imagen
+          src={imagen}
+          alt="Crypto Imagen"
+        />
+      </div>
+      <div>
+        <Heading>
+          Criptomonedas al instante
+        </Heading>
+        <Formulario/>
+      </div>
+    </Contenedor>
   );
 }
 
